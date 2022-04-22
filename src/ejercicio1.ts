074b6837af2164a907e616217b9672176023a07c
arielamaro <104107444+arielamaro@users.noreply.github.com>
2022-04-22 19:55:48 -0300

    Initial commit

---
+++ src/ejercicio1.ts
@@ -0,0 +1,9 @@
+console.log("crear un posteo en facebook");
+console.log("paso2");
+console.log("paso3");
+console.log("paso4");
+console.log("paso5");
+console.log("paso6");
+console.log("paso7");
+console.log("paso8");
+console.log("paso9");
